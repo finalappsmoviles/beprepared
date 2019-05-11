6715843f126ba0c7c5144e0c1c4c451be99eff84
nathalia_ortega <natyan27@hotmail.com>
2019-05-11 17:46:35 -0500

    Creación de las secciones
    Se crean todas las secciones de la aplicación en single page

--- beprepared/www/js/index.js
+++ beprepared/www/js/index.js
@@ -1,11 +1,9 @@
-var menuAzulAbierto, menuAzul,menu, listaPrueba;
+
 var app = {
     // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-        iniciarPrograma();
-        inicializarVariables();
-        asignarEventos();
+        
     },
 
     // deviceready Event Handler
@@ -28,82 +26,7 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-function iniciarPrograma()
-{    
-    var arrayCheck=["0"];
-    if (localStorage.getItem('tareasCompletadas') !== null) {
-        var chequeados=localStorage.getItem('tareasCompletadas');
-        for(var i=1, len=chequeados.length; i<len; i++) {
-            if(i%2==0)
-            {
-                var value = chequeados[i];
-                document.getElementById(value).classList.add('checked');
-                arrayCheck.push(value);
-                localStorage.setItem('tareasCompletadas', arrayCheck);
-            }            
-        }
-    }
-    var listaTareas = document.querySelector('ul');
-    var incrementoAvance=1/listaTareas.childElementCount;
-    var porcentajeTarea;
-    listaTareas.addEventListener('click', function (ev) {
-        if (ev.target.classList.contains('checked')) {
-            ev.target.classList.remove('checked');
-            porcentajeTarea -= incrementoAvance;
-            localStorage.setItem('porcentajeTarea', porcentajeTarea); 
-            arrayCheck = arrayCheck.filter(function(item) { 
-                return item !== ev.target.id
-            })
-            localStorage.setItem('tareasCompletadas', arrayCheck);
-        }
-        else
-        {
-            ev.target.classList.add('checked');
-            if (typeof porcentajeTarea !== 'undefined') {                        
-                porcentajeTarea += incrementoAvance;
-                localStorage.setItem('porcentajeTarea', porcentajeTarea);  
-                arrayCheck.push(ev.target.id);
-                localStorage.setItem('tareasCompletadas', arrayCheck);
-            }
-            else{                        
-                porcentajeTarea = incrementoAvance; 
-                localStorage.setItem('porcentajeTarea', porcentajeTarea);              
-                arrayCheck.push(ev.target.id);
-                localStorage.setItem('tareasCompletadas', arrayCheck);
-            }
-        }
-    },false);
-}
-function inicializarVariables(){
-    menuAzul=document.getElementById("menuAzul");
-    menuAzulAbierto=document.getElementById("menuAzulAbierto");
-    menu=document.getElementById("menu");
-    listaPrueba=document.getElementById("listaPrueba");
-}
-function asignarEventos()
-{
-    mostrarMenu();
-    
-}
-function mostrarMenu()
-{
-    menuAzul.addEventListener("click",function(){
-        menu.className="animated fadeInLeft"; 
-        listaPrueba.className="ocultar";       
-    });
-    menuAzulAbierto.addEventListener("click",function(){
-        menu.className="animated fadeOut"; 
-        menuAzul.className="ocultar";
-        listaPrueba.className="animated fadeInLeft";        
-        setTimeout(function(){ 
-            menu.className="ocultar"; 
-            menuAzul.style.width = "40px";
-            menuAzul.style.height = "40px";
-            menuAzul.style.padding = "5px";
-            menuAzul.className="animated rotateInDownLeft";
-            menuAzulAbierto.src="img/menuAzul.png";            
-        }, 600);
-        
-    });
-}
+
+
+
 app.initialize();
