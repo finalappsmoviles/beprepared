3c503aae48ffbda1cdb56effd2fdb09c0c0dab92
nathalia_ortega <natyan27@hotmail.com>
2019-05-13 14:38:56 -0500

    lista con localstorage
    Marca las atreas realizadas y las guarda en el localStorage

--- beprepared/www/js/index.js
+++ beprepared/www/js/index.js
@@ -49,5 +49,67 @@ function iniciarPrograma()
     //     document.getElementById("Seccarga").className= "Seccarga ocultar";
     //     document.getElementById("Secbienvenidos").className="Secbienvenidos animated fadeInUpBig";
     // }, 2000);
+    
+    var checkList = document.getElementsByTagName("LI");
+    var i;
+    var borradosPruebas,borradosDesafios,borradosEspecialidades;
+    for (i = 0; i < checkList.length; i++) {
+        var span = document.createElement("IMG");
+        span.setAttribute("src", "img/todas/borrar.png");  
+        span.className = "close";
+        checkList[i].appendChild(span);        
+    } 
+    var close = document.getElementsByClassName("close");
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }    
+    }
+
+    var arrayCheck=["0"];
+    if (localStorage.getItem('pruebasCompletas') !== null) {
+        var chequeados=localStorage.getItem('pruebasCompletas');
+        for(var i=1, len=chequeados.length; i<len; i++) {
+            if(i%2==0)
+            {
+                var value = chequeados[i];
+                document.getElementById(value).classList.add('checked');
+                arrayCheck.push(value);
+                localStorage.setItem('pruebasCompletas', arrayCheck);
+            }            
+        }
+    }
+    var listaTareas = document.querySelector('ul');
+    var incrementoAvance=1/listaTareas.childElementCount;
+    var porcentajeTarea;
+    listaTareas.addEventListener('click', function (ev) {
+        if (ev.target.classList.contains('checked')) {
+            ev.target.classList.remove('checked');
+            porcentajeTarea -= incrementoAvance;
+            localStorage.setItem('porcentajeTarea', porcentajeTarea); 
+            arrayCheck = arrayCheck.filter(function(item) { 
+                return item !== ev.target.id
+            })
+            localStorage.setItem('pruebasCompletas', arrayCheck);
+        }
+        else
+        {
+            ev.target.classList.add('checked');
+            if (typeof porcentajeTarea !== 'undefined') {                        
+                porcentajeTarea += incrementoAvance;
+                localStorage.setItem('porcentajeTarea', porcentajeTarea);  
+                arrayCheck.push(ev.target.id);
+                localStorage.setItem('pruebasCompletas', arrayCheck);
+            }
+            else{                        
+                porcentajeTarea = incrementoAvance; 
+                localStorage.setItem('porcentajeTarea', porcentajeTarea);              
+                arrayCheck.push(ev.target.id);
+                localStorage.setItem('pruebasCompletas', arrayCheck);
+            }
+        }  
+        
+    },false);
 }
 app.initialize();
